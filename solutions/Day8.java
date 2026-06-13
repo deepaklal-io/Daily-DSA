c4d5b94f281acac08c5ab4ad74221b6d6a1b727d
deepaklal009 <deepakdoultani00@gmail.com>
2026-06-13 19:49:00 +0500

    Day9

--- solutions/Day8.java
+++ solutions/Day8.java
@@ -1,14 +1,14 @@
 
 
- public class ListNode {
+  class ListNode {
       int val;
       ListNode next;
       ListNode() {}
       ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
   }
 
-class Solution {
+class Day8 {
     public ListNode removeElements(ListNode head, int val) {
         ListNode dummy=new ListNode(0);
         dummy.next=head;
